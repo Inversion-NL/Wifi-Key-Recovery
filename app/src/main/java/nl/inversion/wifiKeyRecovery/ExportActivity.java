49e1891e0ecc0f11b0119deb84b60bd670437f23
nl.inversion@gmail.com <nl.inversion@gmail.com>
2015-03-19 08:23:30 +0100

    Materialize export

--- app/src/main/java/nl/inversion/wifiKeyRecovery/ExportActivity.java
+++ app/src/main/java/nl/inversion/wifiKeyRecovery/ExportActivity.java
@@ -7,13 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import nl.inversion.wifiKeyRecovery.util.UsefulBits;
 
 public class ExportActivity extends Activity {
 	final String TAG =  this.getClass().getName();
+
+    private static final String INTENT_EXPORT_NAME_INFO = "info";
+    private static final String INTENT_EXPORT_NAME_TIME = "time";
 
 	private EditText mFldInfo;
     private String mTimeDate;
@@ -27,45 +31,50 @@ public class ExportActivity extends Activity {
 		final Bundle extras = getIntent().getExtras();
 		mUsefulBits = new UsefulBits(getApplicationContext());
 
-		mFldInfo = (EditText) findViewById(R.id.fld_export_text);
-        Button mBtnShare = (Button) findViewById(R.id.buttonshare);
-        Button mBtnToSd = (Button) findViewById(R.id.buttontosd);
-        Button mBtnClose = (Button) findViewById(R.id.buttoncloseexport);
+        mFldInfo = (EditText) findViewById(R.id.fld_export_text);
 
-		if(extras !=null)
-		{
-			mTimeDate = extras.getString("time");
+		if(extras != null) {
+			mTimeDate = extras.getString(INTENT_EXPORT_NAME_TIME);
 			mFldInfo.setText(getString(R.string.text_wifi_password_recovery)  + " @ " + mTimeDate +"\n\n");
-			mFldInfo.append(extras.getString("info"));
+			mFldInfo.append(extras.getString(INTENT_EXPORT_NAME_INFO));
 		}
 
-		mBtnShare.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                shareResults();
-            }
-        });
-
-		mBtnToSd.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                try {
-                    final File folder = Environment.getExternalStorageDirectory();
-                    final String filename = "wifikeyrecovery_" + mTimeDate + ".txt";
-                    final String contents = mFldInfo.getText().toString();
-                    mUsefulBits.saveToFile(filename, folder, contents);
-                } catch (Exception e) {
-                    Log.e(TAG, "Failed get external storage directory ");
-                    e.printStackTrace();
-                }
-            }
-        });
-
-		mBtnClose.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				finish();
-			}
-		});
 	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.export, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                shareResults();
+                return true;
+
+            case R.id.action_save:
+                writeToSD();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void writeToSD() {
+        try {
+            final File folder = Environment.getExternalStorageDirectory();
+            final String filename = "wifikeyrecovery_" + mTimeDate + ".txt";
+            final String contents = mFldInfo.getText().toString();
+            mUsefulBits.saveToFile(filename, folder, contents);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed get external storage directory ");
+            e.printStackTrace();
+        }
+    }
 
 	private void shareResults(){
 		final Intent sendIntent = new Intent(Intent.ACTION_SEND);
